43b9ee526bb9d56b703662c5272adb6077245293
wolverinepeltz <52587628+wolverinepeltz@users.noreply.github.com>
2021-03-04 16:48:08 -0700

    Update keymap.c
    Updating.

--- keyboards/crkbd/keymaps/wolverine/keymap.c
+++ keyboards/crkbd/keymaps/wolverine/keymap.c
@@ -62,6 +62,7 @@ enum {
 #define NAV_DEL  LT(_NAV,KC_DEL)
 #define NAV_ESC  LT(_NAV,KC_ESC)
 #define NAV_QUOT LT(_NAV,KC_QUOT)
+#define NAV_TAB LT(_NAV,KC_TAB)
 
 #define SFT_SPC SFT_T(KC_SPC)
 #define SFT_BSP SFT_T(KC_BSPC)
@@ -81,7 +82,8 @@ enum {
 #define TD_SCLN_COLN TD(SCLN_COLN)
 //#define TD_GUI_E TD(GUI_E)
 //#define WIN_E LGUI_T(LGUI(KC_E))
-
+#define GUI_DEL LGUI_T(KC_DEL)
+#define OS_SYM OSL(_SYM)
 #define DEFQWE DF(_QWERTY)
 #define DEFCMK DF(_COLEMAK)
 
@@ -91,13 +93,13 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     NAV_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, NAV_QUOT,
+     NAV_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, NAV_QUOT,
   //---------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      OS_CTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM_CS,
   //---------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI, SYM_DEL,   KC_SFTENT,  SFT_SPC,  SYM_BSP, KC_RALT
+                                         GUI_DEL, OS_SYM,   KC_SFTENT,  SFT_SPC,  SYM_BSP, KC_RALT
                                       //|--------------------------|  |--------------------------|
 
 
@@ -105,28 +107,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
   [_COLEMAK] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSLS,
+       KC_ESC,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      NAV_ESC,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, NAV_QUOT,
+      NAV_TAB,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, NAV_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       OS_CTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, OSM_CS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   SYM_DEL, KC_SFTENT,    SFT_SPC,   SYM_BSP, KC_RALT 
+                                          GUI_DEL,   OS_SYM, KC_SFTENT,    SFT_SPC,   SYM_BSP, KC_RALT 
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_SYM] = LAYOUT(
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
-     KC_CIRC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+     KC_TRNS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_UNDS,                         KC_HASH,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_ESC, KC_EXLM, KC_AT,   KC_LPRN, KC_RPRN,  KC_TILD,                      KC_DLR, KC_MINS, KC_EQL, KC_UNDS, KC_COLN, KC_HASH,
+     KC_TRNS, KC_TILD, KC_LPRN,   KC_RPRN, KC_0,  KC_EXLM,                      KC_DLR, KC_1, KC_MINS, KC_EQL, KC_COLN, KC_CIRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	 KC_TRNS, KC_LCBR,  KC_RCBR, KC_LBRC, KC_RBRC, KC_AMPR,                      KC_PERC, KC_PLUS, KC_ASTR,   KC_DOT2,   KC_SLSH2, KC_TRNS,
+	 KC_LCBR, KC_RCBR,  KC_LBRC, KC_LBRC, KC_AT, KC_AMPR,                      KC_PERC, KC_ASTR, KC_PLUS,   KC_DOT2,   KC_SLSH2, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 										 KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
                                       //|--------------------------|  |--------------------------|
   ),
 
+   // [_SYM] = LAYOUT(
+  //|-----------------------------------------------------|                    |-----------------------------------------------------|
+   //  KC_CIRC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+ //    KC_ESC, KC_EXLM, KC_AT,   KC_LPRN, KC_RPRN,  KC_TILD,                      KC_DLR, KC_MINS, KC_EQL, KC_UNDS, KC_COLN, KC_HASH,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//	 KC_TRNS, KC_LCBR,  KC_RCBR, KC_LBRC, KC_RBRC, KC_AMPR,                      KC_PERC, KC_PLUS, KC_ASTR,   KC_DOT2,   KC_SLSH2, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+///										 KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
+                                      //|--------------------------|  |--------------------------|
+  //),
+  
   [_NAV] = LAYOUT(
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
      DEFQWE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_PGUP, PREV_WD, KC_UP,   NEXT_WD, KC_HOME, KC_NO, 
